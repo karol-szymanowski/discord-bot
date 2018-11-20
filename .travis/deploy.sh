52f13cb9078ae8bd4a72eba6df8f937890f1408c
tetrash <szyma.karol@gmail.com>
2018-11-20 22:20:39 +0100

    Fix deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,3 @@
 set -e
-ssh -i ~/.ssh/id_rsa -p ${port} ${user}@${host} pwd
-cd ~/discord-bot
-git pull
-npm i
-pm2 restart discord-bot
+ssh -i ~/.ssh/id_rsa -p ${port} ${user}@${host} pwd && cd ~/discord-bot && git pull && npm i && pm2 restart discord-bot
 exit
